89d4616405a83ccb2985a7c861b5e3928e443cbf
4bb0n <abbonpham245@gmail.com>
2025-03-27 09:08:09 +1100

    Fix get more upgrades than the allowed value

--- eventListeners.js
+++ eventListeners.js
@@ -2,7 +2,7 @@
 document.getElementById("healthBoost").addEventListener("click", () => {
     const healthBoost = document.getElementById("healthBoost");
     
-    if(healthBoost.value < healthBoost.max){
+    if(healthBoost.value < healthBoost.max && upgradesLeft > 0){
     tank.health += 15;
     document.getElementById("healthBoost").value += 1;
     upgradesLeft -= 1
@@ -11,7 +11,7 @@ document.getElementById("healthBoost").addEventListener("click", () => {
 })
 document.getElementById("reload").addEventListener("click", () => {
     clearInterval(bulletInterval);
-    if(document.getElementById("reload").value < document.getElementById("reload").max){
+    if(document.getElementById("reload").value < document.getElementById("reload").max && upgradesLeft > 0){
     reloadSpeed -= 10;
     document.getElementById("reload").value += 1;
     upgradesLeft -= 1
@@ -23,7 +23,7 @@ document.getElementById("reload").addEventListener("click", () => {
 })
 document.getElementById("bulletDamage").addEventListener("click", () => {
     const bulletDamage = document.getElementById("bulletDamage");
-    if(bulletDamage.value < bulletDamage.max){
+    if(bulletDamage.value < bulletDamage.max && upgradesLeft > 0){
     document.getElementById("bulletDamage").value += 1;
     damage += 1
     upgradesLeft -= 1
@@ -33,7 +33,7 @@ document.getElementById("bulletDamage").addEventListener("click", () => {
 
 document.getElementById("bodyDamage").addEventListener("click", () => {
     const bodyDamage = document.getElementById("bodyDamage");
-    if(bodyDamage.value < bodyDamage.max){
+    if(bodyDamage.value < bodyDamage.max && upgradesLeft > 0){
     document.getElementById("bodyDamage").value += 1;
     tank.bodyDamageValue += 1
     upgradesLeft -= 1
@@ -53,7 +53,7 @@ document.getElementById("bulletPenetration").addEventListener("click", () => {
 
 document.getElementById("bulletSpeed").addEventListener("click", () => {
     const bulletSpeed2 = document.getElementById("bulletSpeed");
-    if(bulletSpeed2.value < bulletSpeed2.max){
+    if(bulletSpeed2.value < bulletSpeed2.max && upgradesLeft > 0){
     document.getElementById("bulletSpeed").value += 1;
     bulletSpeed += 1
     upgradesLeft -= 1
@@ -62,7 +62,7 @@ document.getElementById("bulletSpeed").addEventListener("click", () => {
 })
 document.getElementById("movementSpeed").addEventListener("click", () => {
     const movementSpeed2 = document.getElementById("movementSpeed");
-    if(movementSpeed2.value < movementSpeed2.max){
+    if(movementSpeed2.value < movementSpeed2.max && upgradesLeft > 0){
     document.getElementById("movementSpeed").value += 1;
     tank.speed += 0.5
     upgradesLeft -= 1
